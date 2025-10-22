bba47c43ec0b18aa8c0b3ab83d78e5e12015bb55
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-22 16:57:13 +0200

    add some more examples of CompletableFutures

--- CompletableFuture/Main.java
+++ CompletableFuture/Main.java
@@ -1,9 +1,13 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     
     public static void main (String [] args) {
-        CompletableFutureThenAcceptBoth.example();
+        CompletableFutureRunnableExecutor.example();
     }
 
 }
@@ -118,6 +122,100 @@ class CompletableFutureThenAcceptBoth {
                 .thenAcceptBoth( CompletableFuture.supplyAsync(() -> " World!!"),
                     (s1,s2) -> System.out.println(s1 + s2) );
         completableFuture.join();
+    }
+
+}
+
+class CompletableFutureAllOf {
+
+    public static void example() {
+        CompletableFuture<String> future1 =
+            CompletableFuture.supplyAsync(() -> "Hello");
+        
+        CompletableFuture<String> future2 = 
+            CompletableFuture.supplyAsync(() -> "Beautiful");
+        
+        CompletableFuture<String> future3 =
+            CompletableFuture.supplyAsync(() ->  "World");
+        
+        CompletableFuture<Void> combinedFuture =
+            CompletableFuture.allOf(future1, future2, future3);
+
+        String combined = 
+            Stream.of(future1, future2, future3)
+                .map(CompletableFuture::join)
+                    .collect(Collectors.joining(" "));
+
+        System.out.println(combined);
+    }
+
+}
+
+class CompletableFutureCompleteExceptionally {
+
+    public static void example() {
+        try {
+            CompletableFuture<Void> completableFuture = 
+                CompletableFuture.runAsync( () -> System.out.println("I'll throw an exception!") );
+            
+            boolean fail = true; // for some reason
+            if (fail) {
+                Boolean completed = completableFuture.completeExceptionally(new Exception("An exception from a CompletableFuture!"));
+            }
+
+            completableFuture.get();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
+
+class CompletableFutureExceptionally {
+
+    public static void example() {
+        CompletableFuture<Integer> completableFuture =
+            CompletableFuture.supplyAsync(() -> Integer.valueOf("3f"))
+                .exceptionally(ex -> {System.out.println("Exception: Some errors occurred!"); return -1;});
+
+        completableFuture.join();
+    }
+
+}
+
+class CompletableFutureHandle {
+
+    public static void example() {
+        CompletableFuture<Integer> completableFuture =
+            CompletableFuture.supplyAsync(() -> Integer.valueOf("3"))
+                .handle( (result, exception) -> {
+                    if (exception != null) {
+                        System.out.println("Exception: some error occurred!");
+                        return -1;
+                    } else {
+                        System.out.println(result);
+                        return result;
+                    }
+                } );
+    }
+
+}
+
+class CompletableFutureRunnableExecutor {
+
+    // runnable return void
+    public static void example() {
+        try {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            CompletableFuture<String> completableFuture = 
+                CompletableFuture.supplyAsync(
+                    () -> "Supplier with complete async", executorService);
+
+            System.out.println(completableFuture.join());
+            executorService.shutdown();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 }

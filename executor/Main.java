ecf523dc04ee5df154fbcf70e1f5dfe9730e9aa5
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-22 11:16:09 +0200

    better example for FixedThreadPool

--- executor/Main.java
+++ executor/Main.java
@@ -3,25 +3,30 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Main {
-    
-    /*
-     * callable
-     * 
-     */
+
     public static void main (String [] args) throws Exception {
+        FixedThreadPool.example();
+    }
 
+}
+
+class SingleThreadExecutor {
+
+    public static void example () throws Exception {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+    }
+
+}
+
+class FixedThreadPool {
+
+    public static void example () throws Exception {
         ExecutorService executoreService = Executors.newFixedThreadPool(2);
-        Future<Integer> future1 = executoreService.submit(() -> {return 8;});
-        Future<Integer> future2 = executoreService.submit(() -> {return 9;});
-
-        Integer res1 = future1.get();
-        Integer res2 = future2.get();
-
-        System.out.println(res1);
-        System.out.println(res2);
+        Future<Void> future1 = executoreService.submit(() -> {Thread.sleep(1000 * 10); System.out.println("future1"); return null;});
+        Future<Void> future2 = executoreService.submit(() -> {Thread.sleep(1000 *  5); System.out.println("future2"); return null;});
+        Future<Void> future3 = executoreService.submit(() -> {System.out.println("future3"); return null;});
 
         executoreService.shutdown();
-
     }
 
 }

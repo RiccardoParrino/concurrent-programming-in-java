53faf12a6902d07f7dfd2c59ffc98ad7b85e51de
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-25 16:10:55 +0200

    add virtual thread spring app

---
+++ virtual-thread-spring-app/src/main/java/com/parrino/riccardo/virtual_thread_spring_app/VirtualThreadSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.parrino.riccardo.virtual_thread_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualThreadSpringAppApplication.class, args);
+	}
+
+}

0f7638006717e01b2668ccdac0315288e6712bb0
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-25 15:53:26 +0200

    add sync app

---
+++ sync-spring-app/src/main/java/com/parrino/riccardo/sync_spring_app/SyncSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.parrino.riccardo.sync_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncSpringAppApplication.class, args);
+	}
+
+}

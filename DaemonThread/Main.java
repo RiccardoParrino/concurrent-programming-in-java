bf35b5e121a7a4d3dbad8600bfb818f19e913656
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-27 12:15:21 +0100

    better daemonThread example

--- DaemonThread/Main.java
+++ DaemonThread/Main.java
@@ -23,6 +23,17 @@ class DaemonExample {
         daemonThread.setDaemon(true);
         daemonThread.start();
 
+        int i = 0;
+        while (i < 100) {
+            i = i + 1;
+            System.out.println("printing: " + i);
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
         System.out.println("Main thread finished.");
     }
 
